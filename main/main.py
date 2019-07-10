1e9909f45b5be9304b2938a320803647d6fc539d
Sulfurbottom <397love@gmail.com>
2019-07-10 14:43:28 +0900

    테스트 함수 추가

--- main/main.py
+++ main/main.py
@@ -1,1 +1,12 @@
-print("Hello SMTP!!")
+import requests
+
+apiKey = "YWK61NRAYQA1IKQ0"
+apiDomain = "https://www.alphavantage.co/query?"
+
+
+def make_request(function_name, symbol):
+    request_url = apiDomain+"function=" + str(function_name)+"&symbol=" + str(symbol)+"&apikey=" + str(apiKey)
+    return request_url
+
+
+print(make_request('TIME_SERIES_MONTHLY_ADJUSTED', 'MSFT'))
