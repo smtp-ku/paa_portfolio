afe81bb01f984bf4604c972fa374bba6658b5ba4
Sulfurbottom <397love@gmail.com>
2019-07-15 17:56:54 +0900

    Ticker 관련 API 작성

--- smtp/paa/models.py
+++ smtp/paa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticker(models.Model):
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    ticker = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class MonthlyPrice(models.Model):
+    price_date = models.DateTimeField()
+    snp = models.FloatField(default=0)
+    nasdaq = models.FloatField(default=0)
+    russell = models.FloatField(default=0)
+    eurostoxx = models.FloatField(default=0)
+    topix = models.FloatField(default=0)
+    mem = models.FloatField(default=0)
+    mar = models.FloatField(default=0)
+    wti_idx = models.FloatField(default=0)
+    agr_idx = models.FloatField(default=0)
+    silver_idx = models.FloatField(default=0)
+    gold_idx = models.FloatField(default=0)
+    high_yield = models.FloatField(default=0)
+    igb = models.FloatField(default=0)
+    ltb = models.FloatField(default=0)
+    skb = models.FloatField(default=0)
--- smtp/smtp/settings.py
+++ smtp/smtp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paa'
+    'paa.apps.PaaConfig'
 ]
 
 MIDDLEWARE = [
