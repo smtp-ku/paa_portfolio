b7a6f0d679ec82900eb8b55323e150f812e17dc6
Sulfurbottom <397love@gmail.com>
2019-07-22 16:44:07 +0900

    Admin 페이지에 Compat  추가

--- smtp/price/admin.py
+++ smtp/price/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Monthly, Daily
+from .models import Monthly, Daily, Compat
 
 admin.site.register(Monthly)
 admin.site.register(Daily)
+admin.site.register(Compat)
