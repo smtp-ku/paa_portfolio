b124358d80e284261221e225a2a7fee509995bd9
Sulfurbottom <397love@gmail.com>
2019-08-05 13:06:39 +0900

    Ticker 테이블 조건에 채권 여부 및 사용여부 추가

--- smtp/ticker/models.py
+++ smtp/ticker/models.py
@@ -5,6 +5,8 @@ class Ticker(models.Model):
     code = models.CharField(max_length=20)
     name = models.TextField()
     ticker = models.CharField(max_length=20)
+    isBond = models.BooleanField(default=False)
+    isEnabled = models.BooleanField(default=True)
     description = models.TextField()
 
     def __str__(self):
